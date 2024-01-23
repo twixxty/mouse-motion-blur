8bac4024728b4ad7db3d9acea82e07cff4fbc94e
gixxty <151402590+twixxty@users.noreply.github.com>
2024-01-23 14:18:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+var $box = $('.box'),
+  inter = 30,
+  speed = 0;
+
+function moveBox(e) {
+  //TweenMax.killTweensOf();
+  $box.each(function(index, val) {
+   TweenLite.to($(this), 0.05, { css: { left: e.pageX, top: e.pageY},delay:0+(index/750)});
+  });
+}
+
+$(window).on('mousemove', moveBox);
+
+$box.each(function(index, val) {
+    index = index + 1;
+    TweenMax.set(
+      $(this),{
+        autoAlpha: 1,
+        delay:0
+      });
+  });
+  TweenMax.set(
+    $('.text:nth-child(30)'), {
+      autoAlpha: 1,
+      delay: 0
+    }
+  );
+
+//V2.0
+$('.pointer').mouseover(function(){
+  //console.log('cursor');
+  $box.addClass('arrow');
+});
+$('.circle').mouseover(function(){
+  //console.log('circle');
+  $box.removeClass('arrow');
+});
